771faece1f6c3c483243858ef1531f113fba1173
nmathew6 <41180186+nmathew6@users.noreply.github.com>
2018-07-14 00:42:45 -0700

    Update tictactoe.py
    Modified class to be more object oriented. Initialized variables in methods.

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,8 @@
 class TicTacToe():
 
-    def __init__(self):
+    def __init__(self, letter):
         self.board = [' '] * 10
+        self.letter = letter
 
     def drawBoard(self):
         # This function prints out the board that it was passed.
@@ -19,35 +20,48 @@ class TicTacToe():
         print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
         print('   |   |')
 
-    def playAgain():
+    def playAgain(self):
         # This function returns True if the player wants to play again, otherwise it returns False.
         print('Do you want to play again? (yes or no)')
         return input().lower().startswith('y')
 
-    def isWinner(bo, le):
+    def isWinner(self, bo, le):
         # Given a board and a player's letter, this function returns True if that player has won.
         # We use bo instead of board and le instead of letter so we don't have to type as much.
-        return ((bo[7] == le and bo[8] == le and bo[9] == le) or  # across the top
-                (bo[4] == le and bo[5] == le and bo[6] == le) or  # across the middle
-                (bo[1] == le and bo[2] == le and bo[3] == le) or  # across the bottom
-                (bo[7] == le and bo[4] == le and bo[1] == le) or  # down the left side
-                (bo[8] == le and bo[5] == le and bo[2] == le) or  # down the middle
-                (bo[9] == le and bo[6] == le and bo[3] == le) or  # down the right side
-                (bo[7] == le and bo[5] == le and bo[3] == le) or  # diagonal
-                (bo[9] == le and bo[5] == le and bo[1] == le))  # diagonal
+        self.board = bo
+        self.letter = le
+        return ((self.board[7] == self.letter and self.board[8] == self.letter and self.board[9] == self.letter) or  # across the top
+                (self.board[4] == self.letter and self.board[5] == self.letter and self.board[6] == self.letter) or  # across the middle
+                (self.board[1] == self.letter and self.board[2] == self.letter and self.board[3] == self.letter) or  # across the bottom
+                (self.board[7] == self.letter and self.board[4] == self.letter and self.board[1] == self.letter) or  # down the left side
+                (self.board[8] == self.letter and self.board[5] == self.letter and self.board[2] == self.letter) or  # down the middle
+                (self.board[9] == self.letter and self.board[6] == self.letter and self.board[3] == self.letter) or  # down the right side
+                (self.board[7] == self.letter and self.board[5] == self.letter and self.board[3] == self.letter) or  # diagonal
+                (self.board[9] == self.letter and self.board[5] == self.letter and self.board[1] == self.letter))  # diagonal
 
-    def getPlayerMove(board):
+    def getPlayerMove(self, board):
         # Let the player type in his move.
+        self.board = board
         move = ' '
-        while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+        while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(self.board, int(move)):
             print('What is your next move? (1-9)')
             move = input()
         return int(move)
 
-    def isBoardFull(board):
+    def makeMove(self, move):
+        self.move = move
+        self.board[self.move] = self.letter
+
+    def isBoardFull(self):
         # Return True if every space on the board has been taken. Otherwise return False.
         for i in range(1, 10):
-            if isSpaceFree(board, i):
+            if isSpaceFree(self.board, i):
                 return False
         return True
+
+    def isSpaceFree(self, board, move):
+        # Return true if the passed move is free on the passed board.
+        self.board = board
+        self.move = move
+        return self.board[self.move] == ' '
 
