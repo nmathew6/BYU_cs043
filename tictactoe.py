50067fdea4c504d83afeed3bd91e715bae055cd4
nathan <nathangeorgemathew30@gmail.com>
2018-07-13 23:55:52 -0700

    Initial commit

---
+++ tictactoe.py
@@ -0,0 +1,53 @@
+class TicTacToe():
+
+    def __init__(self):
+        self.board = [' '] * 10
+
+    def drawBoard(self):
+        # This function prints out the board that it was passed.
+
+        # "board" is a list of 10 strings representing the board (ignore index 0)
+        print('   |   |')
+        print(' ' + self.board[7] + ' | ' + self.board[8] + ' | ' + self.board[9])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + self.board[4] + ' | ' + self.board[5] + ' | ' + self.board[6])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
+        print('   |   |')
+
+    def playAgain():
+        # This function returns True if the player wants to play again, otherwise it returns False.
+        print('Do you want to play again? (yes or no)')
+        return input().lower().startswith('y')
+
+    def isWinner(bo, le):
+        # Given a board and a player's letter, this function returns True if that player has won.
+        # We use bo instead of board and le instead of letter so we don't have to type as much.
+        return ((bo[7] == le and bo[8] == le and bo[9] == le) or  # across the top
+                (bo[4] == le and bo[5] == le and bo[6] == le) or  # across the middle
+                (bo[1] == le and bo[2] == le and bo[3] == le) or  # across the bottom
+                (bo[7] == le and bo[4] == le and bo[1] == le) or  # down the left side
+                (bo[8] == le and bo[5] == le and bo[2] == le) or  # down the middle
+                (bo[9] == le and bo[6] == le and bo[3] == le) or  # down the right side
+                (bo[7] == le and bo[5] == le and bo[3] == le) or  # diagonal
+                (bo[9] == le and bo[5] == le and bo[1] == le))  # diagonal
+
+    def getPlayerMove(board):
+        # Let the player type in his move.
+        move = ' '
+        while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+            print('What is your next move? (1-9)')
+            move = input()
+        return int(move)
+
+    def isBoardFull(board):
+        # Return True if every space on the board has been taken. Otherwise return False.
+        for i in range(1, 10):
+            if isSpaceFree(board, i):
+                return False
+        return True
+
